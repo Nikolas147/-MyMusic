ab1df5f016fb76c56d4a60ad304cdef39205c754
Nikolas147 <mister.pim97@yandex.ru>
2019-11-06 14:21:48 +0300

    Track View was added

---
+++ MyMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -0,0 +1,129 @@
+//
+//  TrackDetailView.swift
+//  MyMusic
+//
+//  Created by Aleksey on 06/11/2019.
+//  Copyright © 2019 Aleksey. All rights reserved.
+//
+
+import UIKit
+import  SDWebImage
+import AVKit
+
+class TrackDetailView: UIView {
+    
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var currentTimeSlider: UISlider!
+    
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    
+    @IBOutlet weak var trackTitleLabel: UILabel!
+    
+    @IBOutlet weak var authorTitleLabel: UILabel!
+    
+    @IBOutlet weak var playPauseButton: UIButton!
+    
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        
+        return avPlayer
+    }()
+    
+    // MARK: - awakeFromNib
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        let scale: CGFloat = 0.8
+        trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        
+        trackImageView.layer.cornerRadius = 5
+        
+        trackImageView.backgroundColor = .red
+    }
+    
+    // MARK: - Setup
+    
+    func set(viewModel: SearchViewModel.Cell) {
+        trackTitleLabel.text = viewModel.trackName
+        authorTitleLabel.text = viewModel.artistName
+        playTrack(previewUrl: viewModel.previewUrl)
+        monitorStartTime()
+        let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
+        guard let url = URL(string: string600 ?? "") else { return }
+        trackImageView.sd_setImage(with: url, completed: nil)
+        
+    }
+    
+    private func playTrack(previewUrl: String?) {
+        print("try \(previewUrl ?? "not")")
+        
+        guard let url = URL(string: previewUrl ?? "") else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+        
+    }
+    
+    // MARK: - Animatios
+    
+    private func enlargeTrackImageView() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.trackImageView.transform = .identity
+        }, completion: nil)
+    }
+    
+    private func reduceTrackImageView() {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            let scale: CGFloat = 0.8
+            self.trackImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        }, completion: nil)
+    }
+    
+    // MARK: - Time setup
+    
+    private func monitorStartTime() {
+        
+        let time = CMTimeMake(value: 1, timescale: 3)
+        let times = [NSValue(time: time)]
+        player.addBoundaryTimeObserver(forTimes: times, queue: .main) { [weak self] in
+            self?.enlargeTrackImageView()
+        }
+    }
+    // MARK: - @IBActions
+    
+    @IBAction func handleCurrentTimeSlider(_ sender: Any) {
+    }
+    
+    @IBAction func handleVolumeSlider(_ sender: Any) {
+    }
+    
+    @IBAction func dragDownButtonTapped(_ sender: Any) {
+        
+        self.removeFromSuperview()
+    }
+    
+    @IBAction func previousTrack(_ sender: Any) {
+    }
+    
+    @IBAction func nextTrack(_ sender: Any) {
+    }
+    
+    @IBAction func playPauseAction(_ sender: Any) {
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPauseButton.setImage(#imageLiteral(resourceName: "pause"), for: .normal)
+            enlargeTrackImageView()
+        } else {
+            player.pause()
+            playPauseButton.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+            reduceTrackImageView()
+        }
+    }
+    
+    
+}
